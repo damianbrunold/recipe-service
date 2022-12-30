2fd7c88aa4f34b0e0bfd5b00013a9ac4ab37953c
Damian Brunold <dab@dabsoft.ch>
2022-12-30 20:52:37 +0100

    Add dummy routes and error handlers

--- app.py
+++ app.py
@@ -14,14 +14,104 @@ def create_app(test_config=None):
     CORS(app)
     Migrate(app, db)
 
-    @app.route('/')
-    def get_greeting():
+    @app.route('/recipe')
+    def get_recipe_list():
         return "hi there"
 
+    @app.route('/recipe/<int:id>')
+    def get_recipe(recipe_id):
+        return "hi there"
+
+    @app.route('/recipe', methods=("POST",))
+    def add_recipe():
+        return "hi there"
+
+    @app.route('/recipe/<int:id>', methods=("PATCH",))
+    def update_recipe():
+        return "hi there"
+
+    @app.route('/recipe/<int:id>', methods=("DELETE",))
+    def delete_recipe():
+        return "hi there"
+
+    @app.route('/menu')
+    def get_menu_list():
+        return "hi there"
+
+    @app.route('/menu/<int:id>')
+    def get_menu(menu_id):
+        return "hi there"
+
+    @app.route('/menu', methods=("POST",))
+    def add_menu():
+        return "hi there"
+
+    @app.route('/menu/<int:id>', methods=("PATCH",))
+    def update_menu():
+        return "hi there"
+
+    @app.route('/menu/<int:id>', methods=("DELETE",))
+    def delete_menu():
+        return "hi there"
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return (
+            jsonify({
+                'success': False,
+                'error': 400,
+                'message': 'bad request',
+            }),
+            400
+        )
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({
+                'success': False,
+                'error': 404,
+                'message': 'resource not found',
+            }),
+            404
+        )
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return (
+            jsonify({
+                'success': False,
+                'error': 405,
+                'message': 'method not allowed',
+            }),
+            405
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({
+                'success': False,
+                'error': 422,
+                'message': 'unprocessable',
+            }),
+            422
+        )
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return (
+            jsonify({
+                'success': False,
+                'error': 500,
+                'message': 'server error',
+            }),
+            500
+        )
     return app
 
-app = create_app()
 
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
