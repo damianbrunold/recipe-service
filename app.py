4b0b1207bfea99383110e7e2c47137cb2a8dbe1d
Damian Brunold <dab@dabsoft.ch>
2022-12-31 10:12:10 +0100

    Restructure app and improve error handling

--- app.py
+++ app.py
@@ -1,117 +1,81 @@
 import os
 from flask import Flask
+from flask import abort
 from models import setup_db
 from flask_cors import CORS
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 
+from error import setup_error_handlers
+from error import success
+from error import failure
+from error import err_bad_request
+from error import err_unauthorized
+from error import err_forbidden
+from error import err_not_found
+from error import err_method_not_allowed
+from error import err_unprocessable
+from error import err_server_error
+
+
 load_dotenv()
 
-
-def create_app(test_config=None):
-    app = Flask(__name__)
-    db = setup_db(app)
-    CORS(app)
-    Migrate(app, db)
-
-    @app.route('/recipe')
-    def get_recipe_list():
-        return "hi there"
-
-    @app.route('/recipe/<int:id>')
-    def get_recipe(recipe_id):
-        return "hi there"
-
-    @app.route('/recipe', methods=("POST",))
-    def add_recipe():
-        return "hi there"
-
-    @app.route('/recipe/<int:id>', methods=("PATCH",))
-    def update_recipe():
-        return "hi there"
-
-    @app.route('/recipe/<int:id>', methods=("DELETE",))
-    def delete_recipe():
-        return "hi there"
-
-    @app.route('/menu')
-    def get_menu_list():
-        return "hi there"
-
-    @app.route('/menu/<int:id>')
-    def get_menu(menu_id):
-        return "hi there"
-
-    @app.route('/menu', methods=("POST",))
-    def add_menu():
-        return "hi there"
-
-    @app.route('/menu/<int:id>', methods=("PATCH",))
-    def update_menu():
-        return "hi there"
-
-    @app.route('/menu/<int:id>', methods=("DELETE",))
-    def delete_menu():
-        return "hi there"
-
-    @app.errorhandler(400)
-    def bad_request(error):
-        return (
-            jsonify({
-                'success': False,
-                'error': 400,
-                'message': 'bad request',
-            }),
-            400
-        )
-
-    @app.errorhandler(404)
-    def not_found(error):
-        return (
-            jsonify({
-                'success': False,
-                'error': 404,
-                'message': 'resource not found',
-            }),
-            404
-        )
-
-    @app.errorhandler(405)
-    def method_not_allowed(error):
-        return (
-            jsonify({
-                'success': False,
-                'error': 405,
-                'message': 'method not allowed',
-            }),
-            405
-        )
-
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return (
-            jsonify({
-                'success': False,
-                'error': 422,
-                'message': 'unprocessable',
-            }),
-            422
-        )
-
-    @app.errorhandler(500)
-    def server_error(error):
-        return (
-            jsonify({
-                'success': False,
-                'error': 500,
-                'message': 'server error',
-            }),
-            500
-        )
-    return app
+app = Flask(__name__)
+db = setup_db(app)
+setup_error_handlers(app)
+CORS(app)
+Migrate(app, db)
 
 
-app = create_app()
+@app.route('/recipe')
+def get_recipe_list():
+    return "hi there"
+
+
+@app.route('/recipe/<int:recipe_id>')
+def get_recipe(recipe_id):
+    return "hi there"
+
+
+@app.route('/recipe', methods=("POST",))
+def add_recipe():
+    return "hi there"
+
+
+@app.route('/recipe/<int:id>', methods=("PATCH",))
+def update_recipe():
+    return "hi there"
+
+
+@app.route('/recipe/<int:id>', methods=("DELETE",))
+def delete_recipe():
+    return "hi there"
+
+
+@app.route('/menu')
+def get_menu_list():
+    return "hi there"
+
+
+@app.route('/menu/<int:id>')
+def get_menu(menu_id):
+    return "hi there"
+
+
+@app.route('/menu', methods=("POST",))
+def add_menu():
+    return "hi there"
+
+
+@app.route('/menu/<int:id>', methods=("PATCH",))
+def update_menu():
+    return "hi there"
+
+
+@app.route('/menu/<int:id>', methods=("DELETE",))
+def delete_menu():
+    return "hi there"
+
 
 if __name__ == '__main__':
     app.run()
---
+++ error.py
@@ -0,0 +1,89 @@
+from flask import abort
+from flask import jsonify
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def success(key, payload):
+    return (jsonify({"success": True, key: payload}), 200)
+
+
+def failure(message):
+    return (jsonify({"success": False, "message": message}), 200)
+
+
+def err_bad_request(msg):
+    abort(400, description=msg)
+
+
+def err_unauthorized(msg):
+    abort(401, description=msg)
+
+
+def err_forbidden(msg):
+    abort(403, description=msg)
+
+
+def err_not_found(msg):
+    abort(404, description=msg)
+
+
+def err_method_not_allowed(msg):
+    abort(405, description=msg)
+
+
+def err_unprocessable(msg):
+    abort(422, description=msg)
+
+
+def err_server_error(msg):
+    abort(500, description=msg)
+
+
+def setup_error_handlers(app):
+    @app.errorhandler(400)
+    def bad_request(error):
+        return generic_error(400, error)
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return generic_error(401, error)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return generic_error(403, error)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return generic_error(404, error)
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return generic_error(405, error)
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return generic_error(422, error)
+
+    @app.errorhandler(500)
+    def unprocessable(error):
+        return generic_error(500, error)
+
+    @app.errorhandler(AuthError)
+    def auth_error(error):
+        return generic_error(error.status_code, error.error)
+
+
+def generic_error(status_code, error):
+    return (
+        jsonify({
+            'success': False,
+            'error': status_code,
+            'message': str(error),
+        }),
+        status_code
+    )
